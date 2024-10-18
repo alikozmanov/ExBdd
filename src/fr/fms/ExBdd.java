ace56a55a17afc0de02ac6906c31da1bb399aac9
Ozmanov Alik <alik.ozmanov@fms-ea.com>
2024-10-18 15:27:34 +0200

    Initial commit

--- src/fr/fms/ExBdd.java
+++ src/fr/fms/ExBdd.java
@@ -12,30 +12,35 @@ import fr.fms.entities.Article;
 
 public class ExBdd {
 	public static void main(String[] args){
-		ArrayList<Article> articles = new ArrayList<Article>();
-		Article obj1 = new Article("Manette", "Dualshock", 59.99);
+		ArrayList<Article> articles = new ArrayList<Article>(); // Création d'une liste pour stocker les articles
+		Article obj1 = new Article("Manette", "Dualshock", 59.99); // Création d'un article
 		Article obj2 = new Article(22, 700); // Mettre à jour l'article avec l'Id et son prix
-		Article obj3 = new Article(22);
+		Article obj3 = new Article(22); // La suppression en fonction de l'ID
 		String strSql = "SELECT * FROM T_Articles";
 		String str1 = "INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( '"+obj1.getDescription()+"' , '"+obj1.getBrand()+"', "+obj1.getUnitaryPrice()+")";
 		String str2 = "UPDATE T_Articles SET UnitaryPrice = "+obj2.getUnitaryPrice()+" WHERE IdArticle = "+obj2.getIdArticle()+"";
 		String str3 = "DELETE FROM T_Articles WHERE IdArticle = "+obj3.getIdArticle()+"";
 		String str4 = "SELECT * FROM T_Articles WHERE IdArticle =4";
+		
 		try {
-			Class.forName("org.mariadb.jdbc.Driver");
+			Class.forName("org.mariadb.jdbc.Driver"); // Chargement du driver de la BDD
 		}
 		catch(ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		// Paramètres de connexion à la BDD
 		String url = "jdbc:mariadb://localhost:3306/shop";
 		String login = "root";
 		String password = "fms2024";
+		
 		try(Connection connection = DriverManager.getConnection(url, login, password)){
 			Statement statement = connection.createStatement();
-				// Requete d'insertion
+				// Requete d'insertion ici
 				statement.execute(str1);
 				ResultSet resultSet = statement.executeQuery(strSql);				
 					
+				// Parcours des résultats de la requete et ajout des articles dans la liste
 					while(resultSet.next()) {
 						int rsIdUser = resultSet.getInt(1);
 						String rsDescription = resultSet.getString(2);
@@ -44,18 +49,23 @@ public class ExBdd {
 						articles.add((new Article(rsIdUser, rsDescription, rsBrand, rsPrice)));
 					}
 					
+					// Execution de la requete de sélection pour un article spécifique
 					select(obj2, str4, statement);
-			articles.forEach((a) -> {System.out.println(a.getIdArticle()+ " - " +a.getDescription()+ " - " +a.getBrand()+ " - " +a.getUnitaryPrice());});
+					
+					// Affichage des articles dans la console
+					articles.forEach((a) -> {System.out.println(a.getIdArticle()+ " - " +a.getDescription()+ " - " +a.getBrand()+ " - " +a.getUnitaryPrice());});
 			
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
 		}	
 	}
+	// Méthode pour executer des requetes sql d'insertion, mise à jour ou suppression
 	public static void request(Article obj, String str, Statement statement) throws SQLException {
 		int row1 = statement.executeUpdate(str);
 		 if(row1 == 1) System.out.println("réussi");
 	}
+	// MMéthode pour executer une requete de selection et retourner un ResulSet
 	public static ResultSet select(Article obj, String str, Statement statement)  throws SQLException{
 		ResultSet resultSet = statement.executeQuery(str);
 		return resultSet;
---
+++ src/fr/fms/entities/CreateConfigFile.java
@@ -0,0 +1,89 @@
+
+// Permet de gérer la connexion à une base de données en récupérant les informations d'un fichier de configuration, 
+// puis de lire les données d'une table (T_Articles) pour les afficher dans la console.
+
+package fr.fms.entities;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class CreateConfigFile {
+
+    public static void main(String[] args) {
+        ArrayList<Article> articles = new ArrayList<>();
+        String strSql = "SELECT * FROM T_Articles";
+        
+        try {
+            // Crée le fichier de config
+            CreateConfigFile.createFile();
+
+            // Charge les propriétés depuis le fichier de config
+            Properties prop = CreateConfigFile.config("lib/config.properties");
+
+            // Charge la classe du driver JDBC
+            Class.forName(prop.getProperty("db.driver.class"));
+            String url = prop.getProperty("db.url");
+            String login = prop.getProperty("db.login");
+            String password = prop.getProperty("db.password");
+
+            // Connexion à la base de données
+            try (Connection connection = DriverManager.getConnection(url, login, password);
+                 Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(strSql)) {
+
+                // Parcours des résultats de la requête
+                while (resultSet.next()) {
+                    int rsIdUser = resultSet.getInt(1);
+                    String rsDescription = resultSet.getString(2);
+                    String rsBrand = resultSet.getString(3);
+                    double rsPrice = resultSet.getDouble(4);
+                    articles.add(new Article(rsIdUser, rsDescription, rsBrand, rsPrice));
+                }
+
+                // Affiche les articles
+                articles.forEach(a -> System.out.println(
+                        a.getIdArticle() + " - " + a.getDescription() + " - " + a.getBrand() + " - " + a.getUnitaryPrice()));
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Méthode pour charger les propriétés depuis un fichier
+    private static Properties config(String filePath) throws IOException {
+        Properties prop = new Properties();
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            prop.load(fis);
+        }
+        return prop;
+    }
+
+    // Méthode pour créer un fichier de configuration
+    private static void createFile() throws IOException {
+        Properties prop = new Properties();
+        try (OutputStream output = new FileOutputStream("lib/config.properties")) {
+            // Configuration des propriétés de la base de données
+            prop.setProperty("db.driver.class", "org.mariadb.jdbc.Driver");
+            prop.setProperty("db.url", "jdbc:mariadb://localhost:3306/shop");
+            prop.setProperty("db.login", "root");
+            prop.setProperty("db.password", "fms2024");
+
+            // Sauvegarde dans le fichier
+            prop.store(output, null);
+            System.out.println("Fichier de configuration créé avec succès :)");
+        }
+    }
+}
